aa8f54cac78de0719b124c3550c065898c8162f8
jmtt646089 <jadeseala@gmail.com>
2025-12-23 16:48:32 +0800

    import Ant Design

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -6,6 +6,7 @@ import viteLogo from "/vite.svg";
 import cloudflareLogo from "./assets/Cloudflare_Logo.svg";
 import honoLogo from "./assets/hono.svg";
 import "./App.css";
+import { Button, Space, DatePicker, version } from 'antd';
 
 function App() {
 	const [count, setCount] = useState(0);
@@ -55,6 +56,7 @@ function App() {
 				>
 					Name from API is: {name}
 				</button>
+				<Button> This is a Button from Ant Design.</Button>
 				<p>
 					Edit <code>worker/index.ts</code> to change the name
 				</p>
