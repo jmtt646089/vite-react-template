ed45109fba4cbf39bccb565bbfabee8abbf2ef99
jmtt646089 <jadeseala@gmail.com>
2025-12-24 07:54:29 +0800

    Update index.ts

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,6 +1,11 @@
 import { Hono } from "hono";
 const app = new Hono<{ Bindings: Env }>();
 
-app.get("/api/", (c) => c.json({ name: "React-Ant Design-Vite-Hono-CF-JMTT" }));
+app.get("/api/", (c) => c.json({ name: "React-Ant Design-Vite-Honoooooooooooooo-CF-JMTT" }));
+app.get("/products/", (c) => c.json({ name: "Products" }));
+app.get("/productspec/", (c) => c.json({ name: "Product Details by ID" }));
+app.get("/cart/", (c) => c.json({ name: "Shopping Cart" }));
+app.get("/order/", (c) => c.json({ name: "Order" }));
+app.get("/account/", (c) => c.json({ name: "Account" }));
 
 export default app;
