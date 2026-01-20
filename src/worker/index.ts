558acc075bf8de175e532750e36771a857d213a7
jmtt646089 <jadeseala@gmail.com>
2026-01-20 20:51:07 +1300

    Update index.ts

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -3,26 +3,14 @@ import { logger } from 'hono/logger';
 import { createClient } from '@supabase/supabase-js';
 import { env } from "cloudflare:workers";
 
-const app = new Hono<{ Bindings: Env }>();
-
-app.use(logger());
-
-app.get("/api/", (c) => c.json({ name: "React-Ant Design-Vite-Honoooooooooooooo-CF-JMTT" }));
-app.get("/products/", (c) => c.json({ name: "Products" }));
-app.get("/productspec/", (c) => c.json({ name: "Product Details by ID" }));
-app.get("/cart/", (c) => c.json({ name: "Shopping Cart" }));
-app.get("/order/", (c) => c.json({ name: "Order" }));
-app.get("/account/", (c) => c.json({ name: "Account" }));
-
-
-
-
-// Create a single supabase client for interacting with your database
-// Cloudflare workers environment, not hard coding here 
+// Cloudflare workers settings, not bindings 
 const projectUrl = env.PROJECT_URL;
 const publishableKey = env.PUBLISHABLE_KEY;
 
-const supabase = createClient(projectUrl, publishableKey);
+
+function user_ops(url,key) {
+  // Create a single supabase client for interacting with your database
+const supabase = createClient(url, key);
 
 console.log("supabase client created");
 
@@ -39,6 +27,26 @@ await supabase
   .from('users')
   .update({ fname: 'j10noodles' })
   .eq('id', 1);
+    
+}
+
+const app = new Hono<{ Bindings: Env }>();
+
+app.use(logger());
+
+app.get("/api/", (c) => c.json({ name: "React-Ant Design-Vite-Honoooooooooooooo-CF-JMTT" }));
+app.get("/products/", (c) => c.json({ name: "Products" }));
+app.get("/productspec/", (c) => c.json({ name: "Product Details by ID" }));
+app.get("/cart/", (c) => c.json({ name: "Shopping Cart" }));
+app.get("/order/", (c) => c.json({ name: "Order" }));
+app.get("/account/", (c) => c.json({ name: "Account" }));
+app.get("/users/", user_ops(projectUrl,publishableKey));
+
+
+
+
+
+
 
 
 
